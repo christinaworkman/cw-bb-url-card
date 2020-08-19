50a9e46563b1292e523f71c283e7c18ecd99324c
Christina Workman <christinakworkman@gmail.com>
2020-08-19 13:46:36 -0600

    fix dir and url labels

--- cw-url-card/cw-url-card.php
+++ cw-url-card/cw-url-card.php
@@ -10,8 +10,8 @@ class MyUrlCard extends FLBuilderModule {
 					'name'            => __( 'URL Card', 'fl-builder' ),
 					'description'     => __( 'Card layout for urls module!', 'fl-builder' ),
 					'category'        => __( 'CW Modules', 'fl-builder' ),
-					'dir'             => MY_MODULES_DIR . 'cw-url-card/',
-					'url'             => MY_MODULES_URL . 'cw-url-card/',
+					'dir'             => CW_BB_URL_CARD_DIR . 'cw-url-card/',
+					'url'             => CW_BB_URL_CARD_URL . 'cw-url-card/',
 					'icon'            => 'cover-image.svg',
 					'editor_export'   => true, // Defaults to true and can be omitted.
 					'enabled'         => true, // Defaults to true and can be omitted.
