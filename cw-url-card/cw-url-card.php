c7a9e05ccc69ed37bb33411f9b4e8f992c12d279
Aurooba Ahmed <hey@aurooba.com>
2020-08-19 13:32:47 -0600

    Fixed a couple indents and changed the accidental $ in the first line to a ? (#10)

--- cw-url-card/cw-url-card.php
+++ cw-url-card/cw-url-card.php
@@ -1,24 +1,23 @@
-<$php
+<?php
 /**
  * @class FMyUrlCard
  */
  
- class MyUrlCard extends FLBuilderModule {
+class MyUrlCard extends FLBuilderModule {
 
-public function __construct()
-{
-  parent::__construct(array(
-    'name'            => __( 'URL Card', 'fl-builder' ),
-    'description'     => __( 'Card layout for urls module!', 'fl-builder' ),
-    'category'        => __( 'CW Modules', 'fl-builder' ),
-    'dir'             => MY_MODULES_DIR . 'cw-url-card/',
-    'url'             => MY_MODULES_URL . 'cw-url-card/',
-    'icon'            => 'cover-image.svg',
-    'editor_export'   => true, // Defaults to true and can be omitted.
-    'enabled'         => true, // Defaults to true and can be omitted.
-    'partial_refresh' => false, // Defaults to false and can be omitted.
-  ));
-}
+	public function __construct() {
+			parent::__construct(array(
+					'name'            => __( 'URL Card', 'fl-builder' ),
+					'description'     => __( 'Card layout for urls module!', 'fl-builder' ),
+					'category'        => __( 'CW Modules', 'fl-builder' ),
+					'dir'             => MY_MODULES_DIR . 'cw-url-card/',
+					'url'             => MY_MODULES_URL . 'cw-url-card/',
+					'icon'            => 'cover-image.svg',
+					'editor_export'   => true, // Defaults to true and can be omitted.
+					'enabled'         => true, // Defaults to true and can be omitted.
+					'partial_refresh' => false, // Defaults to false and can be omitted.
+			));
+	}
 }
 
 /**
